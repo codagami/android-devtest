7ae096554839bfd15a55bb24a31e8bfdc7dd5c8e
Joseph Hedges <40041080+JoeCodagami@users.noreply.github.com>
2022-11-10 12:11:14 -0600

    Update Constants.kt

--- app/src/main/java/com/codagami/devtest/models/Constants.kt
+++ app/src/main/java/com/codagami/devtest/models/Constants.kt
@@ -5,5 +5,5 @@ object Constants {
     /**
      * Key used by OpenWeather API for validation
      */
-    const val OpenWeatherAPIKey = "fa9f44ae3845a4b0a5feb561a15eff10";
+    const val OpenWeatherAPIKey = "";
 }
